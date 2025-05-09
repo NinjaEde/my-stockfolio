087fc5e014c0506c17776e3cf55be0044c4cfe15
Edgar König <edgar.koenig@gmail.com>
2025-05-09 23:20:21 +0200

    fix: clarify dark mode strategy comment in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // Ensure 'class' strategy is used for dark mode
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
